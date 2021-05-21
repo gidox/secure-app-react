2fb220f36b2867816bdc41c0f0976958eebb094a
Arquimedes <aguiland@assanet.com>
2021-05-21 18:19:07 -0500

    Installed course dependencies

---
+++ src/screens/Home.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default function Home(): React.ReactElement {
+  return (
+    <h1>Home Screen</h1>
+  );
+}
---
+++ src/screens/Profile.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default function Profile(): React.ReactElement {
+  return (
+    <h1>Home Screen</h1>
+  );
+}
